47461e39b38345cbef76c3d7c7a25682c9917561
NikolasAlencar <nikolasalencarrs73@gmail.com>
2024-03-05 09:23:17 -0300

    feat: implementa rotas de navegação

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,19 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, ErrorHandler } from '@angular/core';
+import { provideRouter, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { GlobalErrorHandler } from '../global-error-handler';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes, withViewTransitions()),
+    provideAnimationsAsync(),
+    provideHttpClient(withFetch()),
+    {
+      provide: ErrorHandler,
+      useClass: GlobalErrorHandler
+    }
+  ]
 };
