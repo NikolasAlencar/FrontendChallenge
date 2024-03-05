5c701c9bc6b1e4999dabf29fcaa81c0725147214
NikolasAlencar <nikolasalencarrs73@gmail.com>
2024-03-05 09:25:22 -0300

    feat: cria header

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,20 @@
+import { NgClass } from '@angular/common';
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [MatIconModule, NgClass, RouterLink],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+})
+export class HeaderComponent {
+  activeButton = 'favorites';
+
+  handleButton = (nameButton: string) => (this.activeButton = nameButton);
+
+  isActiveButton = (nameButton: string) =>
+    this.activeButton === nameButton ? 'black' : 'white';
+}
